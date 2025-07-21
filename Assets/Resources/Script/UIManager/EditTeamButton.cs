f471127ab002711f27b2d5a453701716b7097a53
fikrulsmith <56424891+fikrulsmith@users.noreply.github.com>
2025-07-21 21:17:05 +0800

    update scene

--- Assets/Resources/Script/UIManager/EditTeamButton.cs
+++ Assets/Resources/Script/UIManager/EditTeamButton.cs
@@ -1,11 +1,19 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EditTeamButton : MonoBehaviour 
 {
+    public Button myButton;
     public UnitSettingLayout unitSettingLayout;
     public MockPlayerInventoryHolder playerInventoryHolder;
 
-    public void OnClick() 
+    private void Start() {
+        if (myButton != null) {
+            myButton.onClick.AddListener(OnButtonClicked);
+        }
+    }
+
+    public void OnButtonClicked() 
     {
         Debug.Log("Edit Team Button clicked.");
         unitSettingLayout.Init(playerInventoryHolder.playerInventory);
